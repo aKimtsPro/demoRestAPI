36ca75be8f974b7e550a11792234212ebe80b347
akimtspro <alexandre.kimtsaris@outlook.com>
2021-10-12 10:33:40 +0200

    Initial commit

---
+++ src/test/java/bstorm/akimts/api/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bstorm.akimts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
